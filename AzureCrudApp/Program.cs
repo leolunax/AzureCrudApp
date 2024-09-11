ec415ab517b0151afc14265364bdaa82cf20f292
Your Name <your.email@example.com>
2024-09-11 16:58:11 -0700

    hola4

--- AzureCrudApp/Program.cs
+++ AzureCrudApp/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Agregar servicios al contenedor, incluyendo DbContext
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetValue<string>("ConnectionStrings:DefaultConnection")));
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
 
